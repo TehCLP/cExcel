91a56238ffb5cf7b1180f47f28e5f47b4f02d14f
TehCLP <hongtehh@gmail.com>
2016-08-07 15:44:48 +0700

    commit

---
+++ cExcel/excel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.IO;
+
+namespace cExcel
+{
+    public class excel
+    {
+        public DataTable readToDataTable(string path)
+        {
+            string extension = Path.GetExtension(path);
+            DataTable dt = read(path, extension, "string");
+            return dt;
+        }
+        public DataTable readToDataTable(Stream file, string fileName)
+        {
+            DataTable dt = read(file, fileName, "stream");
+            return dt;
+        }
+        private DataTable read(object file, string extension, string type)
+        {
+            string ext = extension.ToLower();
+            if (ext.EndsWith(".xls") || ext.EndsWith(".xlsx"))
+            {
+                try
+                {
+                    dynamic excel;
+                    if (ext.EndsWith(".xls"))
+                        excel = new XLS();
+                    else
+                        excel = new XLSX();
+
+                    dynamic fi;
+                    if (type == "string")
+                        fi = file as string;
+                    else
+                        fi = file as Stream;
+
+                    DataTable dt = new DataTable();
+                    dt = excel.readToDT(fi);
+
+                    return dt;
+                }
+                catch (Exception ex)
+                {
+                    string msg = ex.Message;
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ webTest/Controllers/excelController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.IO;
+using System.Data;
+
+namespace webTest.Controllers
+{
+    public class excelController : Controller
+    {
+        public ActionResult read()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult read(DataTable model)
+        {
+            DataTable dt = new DataTable();
+
+            string _path = Request["path"];
+
+            if (!string.IsNullOrEmpty(_path))
+            {
+                _path = Server.MapPath(_path);
+
+                cExcel.excel ex = new cExcel.excel();
+                dt = ex.readToDataTable(_path);
+            }
+            else if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    //var path = Path.Combine(Server.MapPath("~/files/"), fileName);
+                    //file.SaveAs(path);
+
+                    cExcel.excel ex = new cExcel.excel();
+                    dt = ex.readToDataTable(file.InputStream, fileName);
+                }
+            }
+
+            return View(dt);
+        }
+
+    }
+}
