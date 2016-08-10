ab1351d9750ff65fff714bae6729c9c8af39e186
chalermporn <chalermporn@silkspan.local>
2016-08-10 12:33:50 +0700

    test

--- webTest/Controllers/excelController.cs
+++ webTest/Controllers/excelController.cs
@@ -66,6 +66,14 @@ namespace webTest.Controllers
 
         public ActionResult create()
         {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "Action", Value = "0" });
+            items.Add(new SelectListItem { Text = "Drama", Value = "1" });
+            items.Add(new SelectListItem { Text = "Comedy", Value = "2", Selected = true });
+            items.Add(new SelectListItem { Text = "Science Fiction", Value = "3" });
+
+            ViewBag.ddl = items;
+
             return View();
         }
 
